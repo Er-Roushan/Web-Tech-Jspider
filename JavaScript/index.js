68be07112abdec9688050f3e1986e60d8400f7df
Roushan Kumar <erroushankrofficial@gmail.com>
2024-08-20 17:02:21 +0530

    js start

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+console.log("Hello this is external js");
+document.write("Helloji");
+document.write("Helloji");
+document.write("Helloji");
+
+//writeln() will add a whitespace character at the end
+document.writeln("helloji");
+document.writeln("helloji");
+document.writeln("helloji");
+
+// window.prompt("What is your age: ")
+window.alert("Heavy Rain");
+window.confirm("Are you sure.");
