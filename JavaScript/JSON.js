ab33fc8734ed814ea35bcab6f0b60154da7bcd15
Roushan Kumar <erroushankrofficial@gmail.com>
2024-09-02 18:01:58 +0530

    file addition

---
+++ JavaScript/JSON.js
@@ -0,0 +1,16 @@
+let emp = {
+    empName: "Roushan Kumar",
+    eid : 1,
+    salary :150000,
+    age : 40,
+}
+
+console.log(emp);
+console.log(JSON.stringify(emp));
+
+console.log(JSON.parse(JSON.stringify(emp)));
+
+
+
+
+
--- JavaScript/String.js
+++ JavaScript/String.js
@@ -3,15 +3,32 @@
     // 2) By using single quotes
     // 3) By using back ticks ``
 
-    let a = window.prompt("Enter Your Name: ");
-    console.log(`Your name is ${a}`);
+    // let a = window.prompt("Enter Your Name: ");
+    // console.log(`Your name is ${a}`);
 
 
-    let str = "This is javaScript";
-    let str1 = "         This is java";
+    // let str = "This is javaScript";
+    // let str1 = "         This is java";
 
    
-    console.log(str.concat(str1.trimStart()));
+    // console.log(str.concat(str1.trimStart()));
     
 
-    console.log((str.slice(8, str.length)));
+    // console.log((str.slice(8, str.length)));
+
+//padStart() padEnd()
+    let phno = "8677867330";
+        
+    console.log(phno.slice(4,8).padStart(7, "X").padEnd(10, "X"));
+    console.log(phno.slice(0,4).padEnd(10, "X"));
+    console.log(phno.slice(0,4).padEnd(6, "X") + phno.slice(6));
+    console.log(phno.slice(0,4).padEnd(8, "X") + phno.slice(6));
+
+
+
+    localStorage.setItem("data", "Roushan");
+    localStorage.setItem("pwd", "1234");
+    console.log(localStorage.getItem("data"));
+
+    localStorage.removeItem("data");
+    
